1c21565c275efbe8d26aaeb1545b790aed70940e
TGacek89 <gaco.g4@gmail.com>
2022-07-07 20:00:18 +0200

    firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0ISFT1rcL11PNNNmiDH2W_AVRK8HLnL4",
-  authDomain: "photo-blog-e536d.firebaseapp.com",
-  projectId: "photo-blog-e536d",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
   storageBucket: "photo-blog-e536d.appspot.com",
-  messagingSenderId: "51456898328",
-  appId: "1:51456898328:web:3648276f571e24cc0ada6d",
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
